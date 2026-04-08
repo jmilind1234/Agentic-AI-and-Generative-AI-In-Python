04bcc1447bf6545822abf40e7754a9c518eae817
jmilind1234 <jmilind1234@gmail.com>
2026-04-08 15:27:24 +0530

    consumed model directly from hugging face.

---
+++ hf_basic/main.py
@@ -0,0 +1,23 @@
+from transformers import AutoProcessor, AutoModelForImageTextToText
+
+processor = AutoProcessor.from_pretrained("google/gemma-3-4b-it")
+model = AutoModelForImageTextToText.from_pretrained("google/gemma-3-4b-it")
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+            {"type": "text", "text": "What is the name of the girl?"}
+        ]
+    },
+]
+inputs = processor.apply_chat_template(
+	messages,
+	add_generation_prompt=True,
+	tokenize=True,
+	return_dict=True,
+	return_tensors="pt",
+).to(model.device)
+
+outputs = model.generate(**inputs, max_new_tokens=40)
+print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
